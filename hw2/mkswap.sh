95b637f944325bebef93c61ad7089285f12da7f1
VIadIen <94973625+VIadIen@users.noreply.github.com>
2024-02-27 13:07:50 +0300

    Update mkswap.sh

--- hw2/mkswap.sh
+++ hw2/mkswap.sh
@@ -5,4 +5,7 @@ echo -e 'n\np\n\n\n\nt\n82\nw\n' | fdisk /dev/$part
 mkswap /dev/$part'1' | echo -n $(grep -o -E 'UUID.*') >> /etc/fstab && echo ' none swap sw 0 0' >> /etc/fstab
 mount -a
 swapon -a
-#reboot
+lsblk
+echo 'Сервер будет перезагружен через 5 секунд'
+sleep 5
+reboot
